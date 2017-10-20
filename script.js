d4e77912126720e429c2dfc50768fbc162c57bef
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2017-10-19 22:43:47 -0400

    reimplement the solution

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const redBtn = document.querySelector('.js-red-button')
+const blueBtn = document.querySelector('.js-blue-button')
+
+redBtn.addEventListener('click', changeBackgroundColorToRed)
+blueBtn.addEventListener('click', changeBackgroundColorToBlue)
+
 function changeBackgroundColorToBlue() {
   // No need to change the content of this function. Don't worry if this code is new to you.
   document.body.style.backgroundColor = 'blue';
