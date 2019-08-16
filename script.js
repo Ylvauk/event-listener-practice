117381b243f27c75a10e4ee05fb918581dab64e8
Hammad Malik <tomatohammado@gmail.com>
2019-08-16 11:06:54 -0400

    Make variables for Event Targets

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function changeBackgroundColorToBlue() {
   // No need to change the content of this function. Don't worry if this code is new to you.
-  document.body.style.backgroundColor = 'blue';
+  document.body.style.backgroundColor = "blue";
 }
 
 function changeBackgroundColorToRed() {
   // No need to change the content of this function. Don't worry if this code is new to you.
-  document.body.style.backgroundColor = 'red';
+  document.body.style.backgroundColor = "red";
 }
+
+// I want to store my Event Targets in variables
+
+const redButton = document.querySelector("button.red.button.js-red-button");
+const blueButton = document.querySelector("button.blue");
